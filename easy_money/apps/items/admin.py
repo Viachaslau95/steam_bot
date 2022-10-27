1293f84e7ae2c638c4a9942775ac7533af17e33d
v.kulchynski <6891094@mail.ru>
2022-10-27 12:31:15 +0300

    delete field price_3

--- easy_money/apps/items/admin.py
+++ easy_money/apps/items/admin.py
@@ -5,5 +5,5 @@ from apps.items.models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['title', 'link', 'price_3', 'price_4', 'is_active']
+    list_display = ['title', 'link', 'price_4', 'is_active']
     list_editable = ['is_active']
--- easy_money/apps/items/models.py
+++ easy_money/apps/items/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=255)
-    price_3 = models.FloatField()
     price_4 = models.FloatField()
     is_active = models.BooleanField()
     is_testing = models.BooleanField()
